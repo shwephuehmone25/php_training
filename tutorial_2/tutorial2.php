a68e8e344ccdddd085a3bf1293e5ff31418221e7
shwephuehmone25 <shwephue7889@gmail.com>
2022-04-21 18:21:51 +0630

    Commit message

--- tutorial_2/tutorial2.php
+++ tutorial_2/tutorial2.php
@@ -1,24 +1,38 @@
-<?php  
-for($i=11; $i>=1; $i--)  
-{  
-if($i%2 != 0)  
-{  
-for($j=11; $j>=$i; $j--)  
-{  
-echo "* ";  
-}  
-echo "<br>";  
-}  
-}  
-for($i=2; $i<=11; $i++)  
-{  
- if($i%2 != 0)  
-{  
- for($j=11; $j>=$i; $j--)  
-{  
-echo "* ";  
-}  
-echo "<br>";  
-}  
-}  
-?>  
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial02</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="container">
+   
+    <?php 
+        $n = 6;
+        for ($row = 1; $row <= $n; $row++) {
+            for ($i = 1; $i <= (2 * $n) - 1; $i++) {
+                if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
+                    echo "* ";
+                } else {
+                    echo " &nbsp;&nbsp; ";
+                }
+            }
+            echo "<br/>";
+        }
+
+        for ($row = ($n - 1); $row >= 1; $row--) {
+            for ($i = 1; $i <= (2 * $n) - 1; $i++) {
+                if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
+                    echo "* ";
+                } else {
+                    echo " &nbsp;&nbsp; ";
+                }
+            }
+            echo "<br/>";
+        }
+    ?>
+</body>
+</html>
