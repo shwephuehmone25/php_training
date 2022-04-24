902fbc26cc03748f634a2ced06fc4ba23c350789
shwephuehmone25 <shwephue7889@gmail.com>
2022-04-24 10:42:55 +0630

    commit message

--- tutorial_3/tutorial3.php
+++ tutorial_3/tutorial3.php
@@ -3,35 +3,40 @@
 <head>
 	<meta charset="utf-8">
 	<title>Tutorial03</title>
-	
-</head>
+	<link rel="stylesheet" href="css/style.css">
+	</head>
 <body>
-<div class="container">
-	<h1 class="page-header text-center">Tutorial03</h1>
-	<div class="row">
-		<div class="col-sm-4 col-sm-offset-4">
-			<form method="POST">
-				<div class="form-group">
-					<label>Birthday:</label>
-					<input type="date" name="birthday" class="form-control" required>
-				</div>
-				<button type="submit" name="calculate" class="btn btn-primary">Calculate</button>
-			</form>
-      <?php
-				if(isset($_POST['calculate'])){
-					$bday = $_POST['birthday'];
+    <div class="container">
+    <h1 class="header">Tutorial03</h1>
+    <div class="row">
+		<div class="blk">
+		<form method="POST">
+    <div class="form-blk">
+    <label>Birthday:</label>
+    <input type="date" name="birthday" class="date" required>
+    </div><br>
+    <button type="submit" name="calculate" class="btn-cal">Calculate</button>
+    </form>
+    		<?php
+		    //calculate age from today date
+				if (isset($_POST['calculate'])){
+
+    			$bday = $_POST['birthday'];
 					$today = date('Y-m-d');
 					$diff = date_diff(date_create($bday), date_create($today));
 					?>
-					<div class="alert alert-info text-center" style="margin-top:20px;">
-						<?php echo 'Age is <b>'.$diff->format('%y').'</b>'; ?>
-					</div>
-					<?php
+
+					<div class="alert-box">
+						<?php 
+						//alert current age
+						echo 'Age is <b>'.$diff->format('%y').'</b>'; 
+						?>
+          </div>
+       	<?php
 				}
- 
-			?>
-		</div>
-	</div>
-</div>
+				?>
+    </div>
+    </div>
+    </div>
 </body>
 </html>
