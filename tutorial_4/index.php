7f8294e4e7a7edf3a293e35d0f25187bce8c5a34
shwephuehmone25 <shwephue7889@gmail.com>
2022-04-24 10:44:11 +0630

    commit message

---
+++ tutorial_4/index.php
@@ -0,0 +1,18 @@
+    <?php
+    //gets session id from cookies, or prepa
+    session_start(); 
+    //if sid exists and login for sid exists
+    if (session_id() == '' || !isset($_SESSION['login'])) 
+    { 
+    ?>
+    <a href="/login.php">Login</a>
+    <?php
+    }
+   else 
+   {
+    echo "Hi, " . $_SESSION['login'];
+    ?>
+    <a href="/logout.php">Logout</a>
+    <?php 
+    }
+    ?>
--- tutorial_4/login.php
+++ tutorial_4/login.php
@@ -1,67 +1,26 @@
 <?php
-   ob_start();
-   session_start();
-?>
-
-<?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
-?>
-
-<html lang = "en">
-   
-   <head>
-      <title>Tutorial04</title>
-      <link rel="stylesheet" href="new.css">
-      
-      <style>
-         
-      </style>
-      
-   </head>
-	
-   <body>
-      
-      <h2>Enter Username and Password</h2> 
-      <div class = "container form-signin">
-         
-         <?php
-            $msg = '';
-            
-            if (isset($_POST['login']) && !empty($_POST['username']) 
-               && !empty($_POST['password'])) {
-				
-               if ($_POST['username'] == 'shwe' && 
-                  $_POST['password'] == '1234') {
-                  $_SESSION['valid'] = true;
-                  $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'shwe';
-                  
-                  echo 'You have entered valid username and password';
-               }else {
-                  $msg = 'Wrong username or password';
-               }
-            }
-         ?>
-      </div> <!-- /container -->
-      
-      <div class = "container">
-      
-         <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-      Enter your UserName: <input type = "text" class = "form-control" 
-               name = "username"   required autofocus></br>
-     Enter your password:<input type = "password" class = "form-control"
-               name = "password"  required><br>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login" >Login</button>
-         </form>
-			
-         
-         Log out <a href = "logout.php" tite = "Logout">Session. 
-      </div> 
-      
-   </body>
-</html>
+session_start();
+const login = 'admin';
+const password = 'admin1234';
+        //when form submitted
+        if (isset($_POST['login']) && isset($_POST['password']))
+        {
+        if ($_POST['login'] === login && $_POST['password'] === password)
+        {
+        //write login to server storage
+        $_SESSION['login'] = $_POST['login'];
+        //redirect to main
+        header('Location: /'); 
+        }
+        else
+        {
+        echo "<script>alert('Wrong login or password');</script>";
+        echo "<noscript>Wrong login or password</noscript>";
+        }
+      }
+        ?>
+    <form method="post">
+    Login:<br><input name="login"><br>
+    Password:<br><input name="password"><br><br>
+    <input type="submit">
+    </form>
