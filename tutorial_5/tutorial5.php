f586f5ff0b37de7bc37eeb09fc1e926d279cda4f
shwephuehmone25 <shwephue7889@gmail.com>
2022-04-27 22:27:54 +0630

    show contents by reading file from directory

--- tutorial_5/tutorial5.php
+++ tutorial_5/tutorial5.php
@@ -1,14 +1,40 @@
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
           <meta charset="UTF-8 encoding">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Tutorial05</title>
-        </head>
-        <body>
+    </head>
+    <body>
+          //for doc
           <?php
-          $csvFile = fopen("sample1.doc", "r");
+          $docFile = fopen("sample1.doc", "r");
+          while (($singleLine = fgetcsv($docFile)) !== false) 
+          {
+	        echo "<tr>";
+	        foreach ($singleLine as $singleTxtLine) {
+		      echo "<td>" . $singleTxtLine . "</td>";
+	        }
+      	echo "</tr>";
+      }
+    fclose($docFile);
+         ?>
+         //for excel
+          <?php
+          $excelFile = fopen("sample2.xlsx", "r");
+          while (($singleLine = fgetcsv($excelFile)) !== false) {
+	        echo "<tr>";
+	        foreach ($singleLine as $singleTxtLine) {
+		      echo "<td>" . $singleTxtLine . "</td>";
+	      }
+      	echo "</tr>";
+      }
+    fclose($excelFile);
+         ?>
+         //for csv
+          <?php
+          $csvFile = fopen("sample.csv", "r");
           while (($singleLine = fgetcsv($csvFile)) !== false) {
 	        echo "<tr>";
 	        foreach ($singleLine as $singleTxtLine) {
@@ -17,6 +43,18 @@
       	echo "</tr>";
       }
     fclose($csvFile);
-    ?>
-    </body>
-        </html>
+         ?>
+         //for text
+          <?php
+          $textFile = fopen("sample3.txt", "r");
+          while (($singleLine = fgetcsv($textFile)) !== false) {
+	        echo "<tr>";
+	        foreach ($singleLine as $singleTxtLine) {
+		      echo "<td>" . $singleTxtLine . "</td>";
+	      }
+      	echo "</tr>";
+      }
+    fclose($textFile);
+         ?>
+      </body>
+      </html>
