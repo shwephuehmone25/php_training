ce0a9260e4875b4c08f06a41ea3f958dd3745c8e
shwephuehmone25 <shwephue7889@gmail.com>
2022-04-27 22:25:31 +0630

    place images in specific diretory

---
+++ tutorial_6/tutorial6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8 encoding">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial06</title>
+</head>
+  <body>
+    <center>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <h3>Select image to upload:</h3>
+    <input class="button button2" type="file" name="fileToUpload" id="fileToUpload" required="required"><br>
+    <input class="button button2" type="submit" value="Upload Image" name="submit">
+    </form>
+    </center>
+  </body>
+</html>
