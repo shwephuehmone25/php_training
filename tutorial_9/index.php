c119c55426a2774938e2743978c0e5942f574e16
shwephuehmone25 <shwephue7889@gmail.com>
2022-05-02 13:38:59 +0630

    Draw graph from tables in php

--- tutorial_9/index.php
+++ tutorial_9/index.php
@@ -1,34 +1,64 @@
-    <?php
-    include "db.php";
-    $qt=mysql_query("select * from customers");
-    header ("Content-type: image/jpg");
-    $x_gap=40; // The gap between each point in y axis
-    $x_max=$x_gap*13; // Maximum width of the graph or horizontal axis
-    $y_max=250; // Maximum  hight of the graph or vertical axis
-    // Above two variables will be used to create a canvas of the image//
-    $im = @ImageCreate ($x_max, $y_max)
-    or die ("Cannot Initialize new GD image stream");
-    $background_color = ImageColorAllocate ($im, 234, 234, 234);
-    $text_color = ImageColorAllocate ($im, 233, 14, 91);
-    $graph_color = ImageColorAllocate ($im,25,25,25);
-    $x1=0;
-    $y1=0;
-    $first_one="yes";
-    while ($nt=mysql_fetch_array($qt))  {
-    //echo "$nt[month], $nt[sales]";
-      $x2=$x1+$x_gap; // Shifting in X axis
-      $y2=$y_max-$nt[sales]; // Coordinate of Y axis
-      ImageString($im,2,$x2,$y2,$nt[month],$graph_color); 
-    //Line above is to print month names on the graph
-    if  ($first_one=="no"){ // this is to prevent from starting $x1= and $y1=0
-      imageline ($im,$x1, $y1,$x2,$y2,$text_color); // Drawing the line between two points
-      }
-      $x1=$x2; // Storing the value for next draw
-      $y1=$y2;
-      $first_one="no"; // Now flag is set to allow the drawing
-      }
-      ImageJPEG ($im);
-
-    ?>
+      <?php
+        $con  = mysqli_connect("localhost","root","1234","crud");
+        if (!$con) {
+        # code...
+       echo "Problem in database connection! Contact administrator!" . mysqli_error();
+        }else{
+         $sql ="SELECT * FROM customers";
+         $result = mysqli_query($con,$sql);
+         $chart_data="";
+         
+        }
+      ?>
+    <!DOCTYPE html>
+    <html lang="en"> 
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Graph</title> 
+    </head>
+    <body>
+        <div style="width:60%;hieght:20%;text-align:center">
+            <h2 class="page-header" >Analytics Reports </h2>
+            <div>Customer Information</div>
+            <canvas  id="chartjs_bar"></canvas> 
+        </div>    
+    </body>
+    <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script type="text/javascript">
+      var ctx = document.getElementById("chartjs_bar").getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels:<?php echo json_encode($productname); ?>,
+                        datasets: [{
+                            backgroundColor: [
+                               "#5969ff",
+                                "#ff407b",
+                                "#25d5f2",
+                                "#ffc750",
+                                "#2ec551",
+                                "#7040fa",
+                                "#ff004e"
+                            ],
+                            data:<?php echo json_encode($sales); ?>,
+                        }]
+                    },
+                    options: {
+                           legend: {
+                        display: true,
+                        position: 'bottom',
+ 
+                        labels: {
+                            fontColor: '#71748d',
+                            fontFamily: 'Circular Std Book',
+                            fontSize: 14,
+                        }
+                    },
+                  }
+                });
+    </script>
+</html>
 
 
