eec7c4c716ac5d61e9000133efab56f2f48fbee2
shwephuehmone25 <shwephue7889@gmail.com>
2022-05-02 14:36:28 +0630

    Age calculator by user input date in php

--- tutorial_3/tutorial3.php
+++ tutorial_3/tutorial3.php
@@ -1,42 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Tutorial03</title>
-	<link rel="stylesheet" href="css/style.css">
-	</head>
-<body>
-    <div class="container">
-    <h1 class="header">Tutorial03</h1>
-    <div class="row">
-		<div class="blk">
-		<form method="POST">
-    <div class="form-blk">
-    <label>Birthday:</label>
-    <input type="date" name="birthday" class="date" required>
-    </div><br>
-    <button type="submit" name="calculate" class="btn-cal">Calculate</button>
-    </form>
-    		<?php
-		    //calculate age from today date
-				if (isset($_POST['calculate'])){
-
-    			$bday = $_POST['birthday'];
-					$today = date('Y-m-d');
-					$diff = date_diff(date_create($bday), date_create($today));
-					?>
-
-					<div class="alert-box">
-						<?php 
-						//alert current age
-						echo 'Age is <b>'.$diff->format('%y').'</b>'; 
-						?>
-          </div>
-       	<?php
-				}
+		<!DOCTYPE html>
+		<html>
+		<head>
+		<meta charset="utf-8">
+		<title>Tutorial03</title>
+		<link rel="stylesheet" href="css/style.css">
+		</head>
+	<body>
+		<div class="container">
+   	 <h1 class="header">Tutorial03</h1>
+   	 <div class="row">
+			<div class="blk">
+			<form method="POST">
+    	<div class="form-blk">
+    	<label>Birthday:</label>
+    	<input type="date" name="birthday" class="date" required>
+    	</div><br>
+    	<button type="submit" name="calculate" class="btn-cal">Calculate</button>
+    	</form>
+				<?php
+		    	//calculate age from today date
+					if (isset($_POST['calculate'])){
+						$bday = $_POST['birthday'];
+						$today = date('Y-m-d');
+						$diff = date_diff(date_create($bday), date_create($today));
+				?>
+		<div class="alert-box">
+				<?php 
+				//alert current age
+					echo 'Age is <b>'.$diff->format('%y').'</b>'; 
 				?>
     </div>
-    </div>
-    </div>
-</body>
-</html>
+		<?php
+			}
+			?>
+    	</div>
+   	 </div>
+    	</div>
+    </body>
+		</html>
