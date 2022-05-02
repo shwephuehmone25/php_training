e5a0044cc4959bdd8038696693175247f5514652
shwephuehmone25 <shwephue7889@gmail.com>
2022-05-02 14:37:41 +0630

    Generate stars pattern in php

--- tutorial_2/tutorial2.php
+++ tutorial_2/tutorial2.php
@@ -1,35 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tutorial02</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body class="container">
-   <?php 
-        $n = 6;
-        for ($row = 1; $row <= $n; $row++) {
-            for ($i = 1; $i <= (2 * $n) - 1; $i++) {
-                if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
-                    echo "* ";
-                } else {
-                    echo " &nbsp;&nbsp; ";
-                }
-            }
-            echo "<br/>";
-        }
-    for ($row = ($n - 1); $row >= 1; $row--) {
-            for ($i = 1; $i <= (2 * $n) - 1; $i++) {
-                if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
-                    echo "* ";
-                } else {
-                    echo " &nbsp;&nbsp; ";
-                }
-            }
-            echo "<br/>";
-        }
-    ?>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Tutorial02</title>
+       <link rel="stylesheet" href="style.css">
+    </head>
+    <body class="container">
+    <!-- row conditions to output descending stars-->
+         <?php 
+            $n = 6;
+            for ($row = 1; $row <= $n; $row++) {
+                for ($i = 1; $i <= (2 * $n) - 1; $i++) {
+                 if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
+                  echo "* ";
+                 } else {
+                     echo " &nbsp;&nbsp; ";
+                 }
+             }
+             echo "<br/>";
+         }
+         //conditions for descending stars
+            for ($row = ($n - 1); $row >= 1; $row--) {
+                for ($i = 1; $i <= (2 * $n) - 1; $i++) {
+                     if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
+                     echo "* ";
+                }   else {
+                 echo " &nbsp;&nbsp; ";
+                 }
+             }
+             echo "<br/>";
+         }
+     ?>
+    </body>
+    </html>
