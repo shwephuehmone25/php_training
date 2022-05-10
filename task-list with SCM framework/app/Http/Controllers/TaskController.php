96be125cab1bf4e1705b0ce22348329a217e8fb4
shwephuehmone25 <shwephue7889@gmail.com>
2022-05-10 19:39:58 +0630

    Import File & Export Download

--- task-list with SCM framework/app/Http/Controllers/TaskController.php
+++ task-list with SCM framework/app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use App\Contracts\Services\Task\TaskServiceInterface;
 use Illuminate\Http\Request;
 use App\Models\Todolist;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
@@ -23,19 +24,24 @@ class TaskController extends Controller
     {
         $this->taskInterface = $taskServiceInterface;
     }
-
+    /*select all data from  todolist tables*/
     public function index()
     {
-        $todo = $this->taskInterface->selectTask();
-        info('Index Function' . $todo);
-        return view('index')->with('todolist', $todo);
+        $todo = DB::select('select * from todolist where active = ?', [1]);
+ 
+        return view('index', ['todo' => $todo]);
     }
-
+    /*create a new todolist  in tables*/
     public function create()
     {
         return view('create');
     }
-
+    /*insert data into todolist tables*/
+    public function insert()
+    {
+        DB::insert('insert into todolist (id, name,description) values (?, ?,?)', [2, 'Fixing']);
+    }
+     /*show all data from  todolist tables*/
     public function show(todolist $todo)
     {
         $todo = Todolist::all();
@@ -48,30 +54,17 @@ class TaskController extends Controller
         $todo = Todolist::select('*')->where('id', '=', $id)->get();
         return view('edit')->with('todolist', $todo);
     }
-
+    /*update where name=? from todolist table*/
     public function update(Request $request)
     {
-        try {
-            $this->validate(request(), [
-                'name' => ['required'],
-                'description' => ['required'],
-
-            ]);
-        } catch (ValidationException $e) {
-        }
-
-        Todolist::where('id', '=', $request->id)->update
-                    ([
-                        'name' => $request->name,
-                        'description' => $request->description,
-                    ]);
-        session()->flash('success', 'Todolist updated successfully');
-
+        $affected = DB::update('update todolist set description = demo where name = ?', ['test']);
         return redirect('/');
     }
+
+    /*delete data from todolist table*/
     public function delete($id)
     {
-        Todolist::where('id', '=', $id)->delete();
+        $deleted = DB::delete('delete from todolist');
         return redirect('/');
     }
     public function store()
--- task-list with SCM framework/app/Providers/AppServiceProvider.php
+++ task-list with SCM framework/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            // $query->sql
+            // $query->bindings
+            // $query->time
+        });
     }
 }
