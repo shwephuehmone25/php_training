52ab2732df72a0d4e1521a0121b3688e3cedd9eb
shwephuehmone25 <shwephue7889@gmail.com>
2022-05-11 16:48:33 +0630

    Ajax Request in Laravel

---
+++ Ajax/app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+ 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+ 
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+*
+*
+     * @return void
+     */
+    public function ajaxRequest()
+    {
+        return view('ajaxRequest');
+    }
+ 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function ajaxRequestPost(Request $request)
+    {
+        $input = $request->all();
+        \Log::info($input);
+ 
+        return response()->json(['success'=>'Simple Ajax Request.']);
+    }
+ 
+} 
--- task-list with SCM framework/vendor/composer/installed.php
+++ task-list with SCM framework/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'f9c6be5aca323823748a35559685c0d9d71ea26f',
+        'reference' => '5e4085db3cb9ccf4c78e9ee65ebb1ddf4bc94c29',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -379,7 +379,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'f9c6be5aca323823748a35559685c0d9d71ea26f',
+            'reference' => '5e4085db3cb9ccf4c78e9ee65ebb1ddf4bc94c29',
             'dev_requirement' => false,
         ),
         'laravel/tinker' => array(
@@ -685,8 +685,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
